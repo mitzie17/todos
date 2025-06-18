ea7fce2710f0051ea0cef3ef3200c6f0b10deb71
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-06-17 22:17:55 -0700

    import TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import TodoList from "./TodoList";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Todos</h1>
       <CssBaseline />
+      <h1>Todos</h1>
+      <TodoList />
     </>
   );
 }
