b26ae2977ca5514cb9004caf49fc0e7dcfab695b
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-06-18 22:36:59 -0700

    create TodoForm component

---
+++ src/TodoForm.jsx
@@ -0,0 +1,15 @@
+import ListItem from "@mui/material/ListItem";
+import TextField from "@mui/material/TextField";
+
+const TodoForm = () => {
+  return (
+    <div>
+      TodoForm
+      <ListItem />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <ListItem />
+    </div>
+  );
+};
+
+export default TodoForm;
