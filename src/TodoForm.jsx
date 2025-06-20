79aafd3314935406bf1213492996f5a45e6f724c
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-06-19 22:31:16 -0700

    add TextField component and InputAdornment from MUI

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,12 +1,47 @@
 import ListItem from "@mui/material/ListItem";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Create from "@mui/icons-material/Create";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+
   return (
     <div>
       TodoForm
       <ListItem />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          onChange={handleChange}
+          value={text}
+          slotProps={{
+            input: {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton aria-label="create todo" edge="end" type="submit">
+                    <Create />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </form>
       <ListItem />
     </div>
   );
