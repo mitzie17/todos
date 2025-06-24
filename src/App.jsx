ad8dc739da6891671ffba6cdef7418ffcbc650fe
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-06-23 21:38:39 -0700

    remove h1 and import Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <h1>Todos</h1>
+      <Navbar />
       <TodoList />
     </>
   );
